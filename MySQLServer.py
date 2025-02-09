e32159ba0b4ce91a553b0a0e1697672ae375f287
NAHOM <nahombelayneh387@gmail.com>
2025-02-09 16:55:37 -0500

    added files into intro into database

---
+++ MySQLServer.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        mydb =mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "8910nahom1230",
+            port = "3306"
+        )
+
+        if mydb.is_connected():
+            cursor = mydb.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("DATABASE 'alx_book_store' created successfully or already eXists.")
+
+            cursor.close()
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+    finally:
+
+        if mydb.is_connected():
+            mydb.close()
+            print("MYSQL CONNECTION CLOSED.")
+
+if __name__ == "__main__":
+    create_database()
---
+++ task_4.sql
@@ -0,0 +1,24 @@
+-- task_4.sql
+-- This script retrieves the full description of the 'Books' table
+-- from the 'alx_book_store' database
+
+
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    CHARACTER_MAXIMUM_LENGTH, 
+    NUMERIC_PRECISION, 
+    NUMERIC_SCALE, 
+    DATETIME_PRECISION, 
+    COLUMN_KEY, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
+
+mysql -u your_username -p alx_book_store < task_4.sql
